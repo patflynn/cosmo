ceb1b5cbd3570d5060103e851e19f933531769fe
Patrick Flynn <big.pat@gmail.com>
2016-02-25 11:04:04 -0800

    fix ujelly theme install

--- prelude/personal.el
+++ prelude/personal.el
@@ -1,6 +1,6 @@
 (scroll-bar-mode -1)
 (setq prelude-whitespace nil)
-(prelude-require-packages 'ujelly-theme)
+(prelude-require-packages '(ujelly-theme))
 
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)

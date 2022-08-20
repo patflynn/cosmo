b3d9f2e61e162a0493dc6770c350286a966c3540
Patrick Flynn <big.pat@gmail.com>
2022-08-20 11:03:22 -0400

    add unfree and flake-utils
    Signed-off-by: Patrick Flynn <patrick@chainguard.dev>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,15 @@
 {
-  outputs = { self, nixpkgs }: {
-    # replace 'joes-desktop' with your hostname here.
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+    nixpkgs-unfree.url = "github:numtide/nixpkgs-unfree";
+    nixpkgs-unfree.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ nixpkgs, flake-utils, ... }: {
+
     nixosConfigurations.classic-laddie = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./machines/classic-laddie/default.nix ];

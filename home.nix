b0b9c7fdb69c9364b74d20b2ab2f61e46d792091
Patrick Flynn <big.pat@gmail.com>
2022-08-22 09:08:27 -0400

    add chrome and slack
    Signed-off-by: Patrick Flynn <patrick@chainguard.dev>

--- home.nix
+++ home.nix
@@ -4,11 +4,14 @@
   imports = [
      ./emacs/nixos.nix
      ./code.nix
+     ./git.nix
    ];
   nixpkgs.config.allowUnfree = true;
   home.packages = [
     pkgs.adoptopenjdk-hotspot-bin-16
     pkgs.jetbrains.idea-ultimate
+    pkgs.google-chrome
+    pkgs.slack
   ];
   home.stateVersion = "21.11";
   xdg.configFile."mimeapps.list".force = true;

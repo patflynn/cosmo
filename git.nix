48b7d52c57a3085a4d967cfc05de555a10500a4f
Patrick Flynn <patrick@chainguard.dev>
2022-08-23 10:53:07 -0400

    Add gitsign

--- git.nix
+++ git.nix
@@ -8,6 +8,10 @@
     ignores = [ "*~" "*.swp" ];
     userEmail = "patrick@chainguard.dev";
     userName = "Patrick Flynn";
+    extraConfig.commit.gpgsign = "true";
+    extraConfig.tag.gpgsign = "true";
+    extraConfig.gpg.x509.program = "gitsign";
+    extraConfig.gpg.format = "x509";
     extraConfig.pull.rebase = "true";
     extraConfig.github.user = "patflynn";
     extraConfig.init.defaultBranch = "main";

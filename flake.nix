53398639ea2ff81b77c16f8acb0e4d93a4113f3c
Patrick Flynn <big.pat@gmail.com>
2022-09-19 10:08:40 -0400

    emacsclient and stuff
    Signed-off-by: Patrick Flynn <patrick@chainguard.dev>

--- flake.nix
+++ flake.nix
@@ -10,10 +10,13 @@
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
+    #go-tuf.url = "github.com:znewman01/go-tuf/nix";
+
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
       flake = false;
     };
+
     doom-emacs = {
       url = "github:nix-community/nix-doom-emacs";
       inputs.nixpkgs.follows = "nixpkgs";
--- home.nix
+++ home.nix
@@ -12,9 +12,13 @@
   home.packages = with pkgs; [
     adoptopenjdk-hotspot-bin-16
     # jetbrains.idea-ultimate
+    # go-tuf
     google-chrome
+    #firefox
     google-cloud-sdk
     slack
+    qemu
+    tree
     pavucontrol
     alacritty
     step-cli
@@ -27,11 +31,11 @@
     gcc
     gnumake
     tmux
-    #python310
-    #python310Packages.pip
-    #python310Packages.pip-tools
+    pkg-config
+    jq
+    python3
+    fulcio
     (callPackage ./gitsign.nix {})
-    #(callPackage ./cosign.nix {})
     cosign
     crane
     nodePackages.snyk
@@ -54,6 +58,18 @@
     };
   };
   programs.go.enable = true;
+  programs.tmux = {
+    enable = true;
+    prefix = "C-q";
+    extraConfig = ''
+      set-option -g status-style fg=white,bg=colour233
+      setw -g mouse on
+      set -g base-index 1
+      set -g history-limit 100000
+      set-window-option -g mode-keys emacs
+      unbind-key C-b
+    '';
+  };
   home.stateVersion = "21.11";
   xdg.configFile."mimeapps.list".force = true;
 }

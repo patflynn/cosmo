d004bffcc977f739737411afb4a5c5e5dad0e992
Patrick Flynn <big.pat@gmail.com>
2023-10-16 16:11:35 -0400

    try again to disable sleep

--- nix-basic/configuration.nix
+++ nix-basic/configuration.nix
@@ -16,6 +16,15 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  powerManagement.enable = false;
+
+  # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+  # If no user is logged in, the machine will power down after 20 minutes.
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+  
   networking.hostName = "classic-laddie"; # Define your hostname.
   networking.networkmanager.enable = true;
 

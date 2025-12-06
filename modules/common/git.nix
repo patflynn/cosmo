7b78942bcee93eaeb294036c05bb457ba4ead994
Patrick Flynn <big.pat@gmail.com>
2025-12-06 14:10:48 -0500

    Archive the old, start afresh for the new

--- modules/common/git.nix
+++
@@ -1,9 +0,0 @@
-# Git configuration for NixOS
-{ config, lib, pkgs, ... }:
-
-{
-  # This is adapted from the original git.nix in the repository root
-  environment.systemPackages = with pkgs; [
-    (pkgs.callPackage ../../pkgs/gitsign.nix {})
-  ];
-}

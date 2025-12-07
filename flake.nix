58cf66f68cea379ad390912a95a83d39188ab9c5
Patrick Flynn <big.pat@gmail.com>
2025-12-06 20:15:05 -0500

    add home-manager for patrick

--- flake.nix
+++ flake.nix
@@ -5,9 +5,12 @@
     # The unstable branch is usually fine for desktops/home-servers, 
     # but use "nixos-24.11" if you want rock-solid stability.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
       # Hostname: classic-laddie
       classic-laddie = nixpkgs.lib.nixosSystem {
@@ -15,6 +18,13 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./hosts/classic-laddie/default.nix
+
+	  home-manager.nixosModules.home-manager
+	  {
+ 	    home-manager.useGlobakPkgs = true;
+	    home-manager.useUserPackages = true;
+	    
+	    home-manager.users.patrick = import ./home/server.nix;
         ];
       };
     };

f551a8cf2b90b6811816118f0ecb1ebe98ceec29
Patrick Flynn <big.pat@gmail.com>
2025-12-06 20:41:48 -0500

    fix another typo and make sure that we don't hit the cache when rebuilding

--- home/server.nix
+++ home/server.nix
@@ -39,7 +39,7 @@
     
     shellAliases = {
       ll = "ls -l";
-      update = "sudo nixos-rebuild switch --flake github:patflynn/cosmo#classic-laddie";
+      update = "sudo nixos-rebuild switch --no-write-lock-file --refresh --flake github:patflynn/cosmo#classic-laddie";
     };
   };
 

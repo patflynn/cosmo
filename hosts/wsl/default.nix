ead3c5d82a040911cdd6e6cd8e7ba3fc2f9df2f7
Patrick Flynn <big.pat@gmail.com>
2025-12-08 18:42:50 -0500

    refactor: Centralize user configuration and system settings

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -4,6 +4,7 @@
   imports = [
     # No hardware-configuration.nix needed for WSL
     ../../modules/common/system.nix
+    ../../modules/common/users.nix
   ];
 
   wsl = {
@@ -11,13 +12,6 @@
     defaultUser = "patrick";
     startMenuLaunchers = true;
   };
-
-  # Enable Nix Flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  
-  # Set default shell to zsh system-wide (optional, but good practice if HM configures it)
-  programs.zsh.enable = true;
-  users.users.patrick.shell = pkgs.zsh;
 
   system.stateVersion = "24.11"; 
 }

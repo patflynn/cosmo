e781068273b7244ba8046069c4fcf62713db8c8f
Patrick Flynn <big.pat@gmail.com>
2025-12-08 18:42:50 -0500

    refactor: Centralize user configuration and system settings

--- hosts/johnny-walker/default.nix
+++ hosts/johnny-walker/default.nix
@@ -4,6 +4,7 @@
   imports = [
     # Hardware config is now handled by microvm.nix
     ../../modules/common/system.nix
+    ../../modules/common/users.nix
     ../../modules/common/guest.nix
   ];
 
@@ -15,25 +16,8 @@
   # Set your time zone
   time.timeZone = "America/New_York";
 
-  # Enable zsh system wide
-  programs.zsh.enable = true;
-
-  users.users.patrick = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    extraGroups = [ "wheel" "networkmanager" ];
-    # Use the same hashed password or SSH keys as other hosts
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILc8u2oEFD+sn9vmX0gEbf62V4fmHGSvu10ENPkci3Yd"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHo0Oc728AfV2EMn30DhTWSqdWhmY8xR6np/qf6U7xvn cloud-ssh"
-    ];
-  };
-
   # Enable SSH
   services.openssh.enable = true;
-
-  # Enable Flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   system.stateVersion = "25.11"; 
 }

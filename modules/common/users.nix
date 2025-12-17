07f6e398c22009d3208dd47dec9cbedc12280928
Patrick Flynn <big.pat@gmail.com>
2025-12-17 10:24:38 -0500

    add nixos maker mark wsl2 instance

--- modules/common/users.nix
+++ modules/common/users.nix
@@ -14,6 +14,8 @@
     openssh.authorizedKeys.keys = [
       # makers-mark.ubuntu
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILc8u2oEFD+sn9vmX0gEbf62V4fmHGSvu10ENPkci3Yd"
+      # makers-mark.nixos
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8B2eVhu/TpXZPyOt/6w0ELdtO6X6cTiWz3CvofxDCR"
       # Chrome Secure Shell
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHo0Oc728AfV2EMn30DhTWSqdWhmY8xR6np/qf6U7xvn cloud-ssh"
     ];

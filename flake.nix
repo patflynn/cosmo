4e70aebc7201ef0c4fc691b9a39370248e1bd2ea
Patrick Flynn <big.pat@gmail.com>
2025-12-17 18:02:16 +0000

    fix(ci): format all nix files with nixfmt

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Cosmo: Fresh Start 2025";
 
   inputs = {
-    # The unstable branch is usually fine for desktops/home-servers, 
+    # The unstable branch is usually fine for desktops/home-servers,
     # but use "nixos-24.11" if you want rock-solid stability.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -18,73 +18,81 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-generators, ... }@inputs: {
-    nixosConfigurations = {
-      # Hostname: classic-laddie
-      classic-laddie = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/classic-laddie/default.nix
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      nixos-generators,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        # Hostname: classic-laddie
+        classic-laddie = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/classic-laddie/default.nix
 
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.patrick = import ./home/server.nix;
-          }
-        ];
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.patrick = import ./home/server.nix;
+            }
+          ];
+        };
+
+        # Hostname: wsl
+        wsl = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/wsl/default.nix
+            inputs.nixos-wsl.nixosModules.wsl
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.patrick = import ./home/wsl.nix;
+            }
+          ];
+        };
+
+        # Hostname: johnny-walker
+        johnny-walker = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/johnny-walker/default.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.patrick = import ./home/vm.nix;
+            }
+          ];
+        };
       };
 
-      # Hostname: wsl
-      wsl = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/wsl/default.nix
-          inputs.nixos-wsl.nixosModules.wsl
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.patrick = import ./home/wsl.nix;
-          }
-        ];
-      };
-
-      # Hostname: johnny-walker
-      johnny-walker = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/johnny-walker/default.nix
-
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.patrick = import ./home/vm.nix;
-          }
-        ];
+      packages.x86_64-linux = {
+        johnny-walker-image = nixos-generators.nixosGenerate {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/johnny-walker/default.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.patrick = import ./home/vm.nix;
+            }
+          ];
+          format = "qcow";
+        };
       };
     };
-
-    packages.x86_64-linux = {
-      johnny-walker-image = nixos-generators.nixosGenerate {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/johnny-walker/default.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.patrick = import ./home/vm.nix;
-          }
-        ];
-        format = "qcow";
-      };
-    };
-  };
 }

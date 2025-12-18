bda2a914ff23c255db444cec8ebc919464f0dd53
Patrick Flynn <big.pat@gmail.com>
2025-12-17 20:05:02 -0500

    encrypt user passwords using age

---
+++ secrets/keys.nix
@@ -0,0 +1,15 @@
+{
+  # User Keys
+  users = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILc8u2oEFD+sn9vmX0gEbf62V4fmHGSvu10ENPkci3Yd" # makers-mark.ubuntu
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8B2eVhu/TpXZPyOt/6w0ELdtO6X6cTiWz3CvofxDCR" # makers-mark.nixos
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHo0Oc728AfV2EMn30DhTWSqdWhmY8xR6np/qf6U7xvn cloud-ssh" # Chrome
+  ];
+
+  # Host Keys
+  hosts = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICm/0RSHq33ws9IIlzJyXhL0Gh1eudUE3LZEMUOa6PHX" # wsl
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALgjAYCsxF4HUoW9MmsrShmV45Y1ClM37Z7jXY4Fw+G" # johnny-walker
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE7/mipW9wcQwVlDmEqBZksGDO3BEG94gb6VBuyDJUgk" # classic-laddie
+  ];
+}

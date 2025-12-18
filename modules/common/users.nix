97e4174ee53015d28dd559e79afaf77ba1ad43e6
Patrick Flynn <big.pat@gmail.com>
2025-12-17 20:05:02 -0500

    encrypt user passwords using age

--- modules/common/users.nix
+++ modules/common/users.nix
@@ -1,8 +1,12 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  keys = import ../../secrets/keys.nix;
+in
 {
   programs.zsh.enable = true;
 
+  age.secrets.user-password.file = ../../secrets/user-password.age;
   users.users.patrick = {
     isNormalUser = true;
     shell = pkgs.zsh;
@@ -11,16 +15,8 @@
       "networkmanager"
     ];
 
-    # Shared password hash
-    hashedPassword = "$6$ZtyAYsmFObdDrWxk$t/B4v4b8hHt3gSIjDiLy70fVwrzjjxC9/MRKAWuG/gQqlLZ/PVVclOR1bihX7l/RI8MLPUTS1vjV.ch8tYRb0/";
+    hashedPasswordFile = config.age.secrets.user-password.path;
 
-    openssh.authorizedKeys.keys = [
-      # makers-mark.ubuntu
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILc8u2oEFD+sn9vmX0gEbf62V4fmHGSvu10ENPkci3Yd"
-      # makers-mark.nixos
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8B2eVhu/TpXZPyOt/6w0ELdtO6X6cTiWz3CvofxDCR"
-      # Chrome Secure Shell
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHo0Oc728AfV2EMn30DhTWSqdWhmY8xR6np/qf6U7xvn cloud-ssh"
-    ];
+    openssh.authorizedKeys.keys = keys.users;
   };
 }

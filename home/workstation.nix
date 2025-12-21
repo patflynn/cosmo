3fa9e1d1315a8503f3775cda9b1b0872e9eeb720
Patrick Flynn <big.pat@gmail.com>
2025-12-21 09:05:29 -0500

    feat(ui): set Bibata-Modern-Ice as default cursor theme

--- home/workstation.nix
+++ home/workstation.nix
@@ -15,5 +15,14 @@
     kitty # Terminal
     wofi # App launcher
     kdePackages.dolphin # File manager
+    bibata-cursors
   ];
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    # x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
 }

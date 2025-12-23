f272c6a607d4f298ed5118bd00344ded1df4f880
Patrick Flynn <big.pat@gmail.com>
2025-12-23 02:31:49 +0000

    style: nixfmt workstation.nix

--- modules/common/workstation.nix
+++ modules/common/workstation.nix
@@ -40,21 +40,38 @@
     openFirewall = false; # Restricted to Tailscale below
   };
 
-  # Secure Sunshine: Only allow traffic over the Tailscale interface
-  networking.firewall.interfaces."tailscale0" = {
-    allowedTCPPorts = [
-      47984
-      47989
-      47990
-      48010
-    ];
-    allowedUDPPorts = [
-      47998
-      47999
-      48000
-      48002
-      48010
-    ];
+  # Secure Sunshine: Only allow traffic over trusted interfaces
+  networking.firewall.interfaces = {
+    "tailscale0" = {
+      allowedTCPPorts = [
+        47984
+        47989
+        47990
+        48010
+      ];
+      allowedUDPPorts = [
+        47998
+        47999
+        48000
+        48002
+        48010
+      ];
+    };
+    "enp4s0" = {
+      allowedTCPPorts = [
+        47984
+        47989
+        47990
+        48010
+      ];
+      allowedUDPPorts = [
+        47998
+        47999
+        48000
+        48002
+        48010
+      ];
+    };
   };
 
   # Allow Tailscale to handle direct LAN paths correctly while keeping the local firewall tight.

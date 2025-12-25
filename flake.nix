e9987c07ad3b6b1eb3b668a13116b2cff71ed85e
Patrick Flynn <big.pat@gmail.com>
2025-12-25 17:52:07 -0500

    refactor(bud-lite): complete transition to standalone home-manager (#162)
    * refactor(bud-lite): define aliases in flake.nix, keep dev.nix pure
    
    * refactor(home): extract crostini specific config into home/crostini.nix

--- flake.nix
+++ flake.nix
@@ -94,17 +94,35 @@
       };
 
       homeConfigurations = {
+
         "patrick@bud-lite" = home-manager.lib.homeManagerConfiguration {
+
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          extraSpecialArgs = { inherit inputs; };
+
+          extraSpecialArgs = {
+
+            inherit inputs;
+
+            hostName = "bud-lite";
+
+          };
+
           modules = [
-            ./home/dev.nix
+
+            ./home/crostini.nix
+
             {
+
               home.username = "patrick";
+
               home.homeDirectory = "/home/patrick";
+
             }
+
           ];
+
         };
+
       };
 
       packages.x86_64-linux = {

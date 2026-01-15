9dce62fa9374533414d08bf766c97ffeed127f50
Patrick Flynn <big.pat@gmail.com>
2026-01-14 21:35:55 -0500

    feat(classic-laddie): configure 16GB ZFS swap volume

--- hosts/classic-laddie/hardware-configuration.nix
+++ hosts/classic-laddie/hardware-configuration.nix
@@ -51,7 +51,9 @@
     ];
   };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/dev/zvol/rpool/swap"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

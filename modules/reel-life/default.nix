f2d451cb80a7000445ea690d9e9417a2dcfd7e52
Patrick Flynn <big.pat@gmail.com>
2026-04-01 16:04:35 -0400

    Fix reel-life microVM interface type to allow bridge attribute
    The microvm.nix schema requires type = "bridge" (not "tap") when the
    bridge attribute is set. Change the interface type so the flake check
    passes.

--- modules/reel-life/default.nix
+++ modules/reel-life/default.nix
@@ -46,7 +46,7 @@
     # TAP network interface
     interfaces = [
       {
-        type = "tap";
+        type = "bridge";
         id = "vm-reel-0";
         mac = "02:00:00:00:01:01";
         bridge = "br-reel";
